42ba90c883e10d1bed5004c05d86325e4ff80fbd
cyberprophet <cyberprophet@hanmail.net>
2023-03-14 12:43:02 +0900

    implementing login text form fields

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:sbas/screens/login_widgets/login_form.dart';
 
-class LogInScreen extends StatelessWidget {
+class LogInScreen extends StatefulWidget {
+  @override
+  State<LogInScreen> createState() => LogInScreenState();
+
   const LogInScreen({super.key});
 
+  static String routeName = 'login';
+  static String routeUrl = '/login';
+}
+
+class LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,68 +24,71 @@ class LogInScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 20,
-            ),
-            child: Icon(
-              Icons.login,
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 30,
           ),
-          const LoginForm(),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 20,
-            ),
-            child: Container(
-              color: Colors.amber,
-              child: const Text(
-                'recaptcha',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              '로그인',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          child: Column(
             children: [
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  '아이디 찾기',
+              const Icon(
+                Icons.login,
+              ),
+              const LoginForm(),
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 20,
                 ),
+                color: Colors.amber,
+                child: const Text(
+                  'recaptcha',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '로그인',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      '아이디 찾기',
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      '비밀번호 재설정',
+                    ),
+                  ),
+                ],
               ),
               TextButton(
                 onPressed: () {},
                 child: const Text(
-                  '비밀번호 재설정',
+                  '사용자 등록 요청',
                 ),
               ),
             ],
           ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              '사용자 등록 요청',
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 
-  static String routeName = 'login';
-  static String routeUrl = '/login';
+  final Map<String, String> formData = {};
 }
