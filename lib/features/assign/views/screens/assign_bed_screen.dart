40b32599ecb50262161be0b7f25c484d1825588b
cyberprophet <cyberprophet@hanmail.net>
2023-03-21 19:00:11 +0900

    implementation of the top animation navbar

--- lib/features/assign/views/screens/assign_bed_screen.dart
+++ lib/features/assign/views/screens/assign_bed_screen.dart
@@ -2,78 +2,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sbas/common/bitflow_theme.dart';
 import 'package:sbas/constants/gaps.dart';
+import 'package:sbas/features/assign/views/widgets/top_navbar_widget.dart';
 import 'package:sbas/features/assign/views/widgets/top_search_widget.dart';
 
-class AssignBedScreen extends ConsumerStatefulWidget {
+class AssignBedScreen extends ConsumerWidget {
   const AssignBedScreen({
     super.key,
     required this.automaticallyImplyLeading,
   });
 
   @override
-  ConsumerState<AssignBedScreen> createState() => _AssignBedScreenState();
-
-  final bool automaticallyImplyLeading;
-}
-
-class _AssignBedScreenState extends ConsumerState<AssignBedScreen> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: Bitflow.getAppBar(
         '병상 배정 현황',
-        widget.automaticallyImplyLeading,
+        automaticallyImplyLeading,
         0.5,
       ),
       body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
         child: Column(
-          children: const [
-            Padding(
+          children: [
+            const Padding(
               padding: EdgeInsets.symmetric(
                 vertical: 24,
                 horizontal: 16,
               ),
               child: TopSearch(),
             ),
-            Divider(
+            const Divider(
               color: Colors.grey,
               height: 1,
             ),
-            /*
-            Row(
-              children: [
-                Container(
-                  child: Row(
-                    children: const [
-                      Text(
-                        '병상요청',
+            const Padding(
+              padding: EdgeInsets.fromLTRB(
+                16,
+                14,
+                16,
+                0,
+              ),
+              child: TopNavbar(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Container(
+                alignment: Alignment.topLeft,
+                child: RichText(
+                  text: const TextSpan(
+                    text: '총',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    children: [
+                      WidgetSpan(
+                        child: Gaps.h1,
+                      ),
+                      TextSpan(
+                        text: '17',
                         style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
                           fontSize: 18,
+                        ),
+                      ),
+                      TextSpan(
+                        text: '명',
+                        style: TextStyle(
                           color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
                         ),
-                      ),
-                      Gaps.h1,
-                      CircleAvatar(
-                        backgroundColor: Colors.lightBlue,
-                        maxRadius: 10,
-                        child: Text(
-                          '7',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ),
+                      )
                     ],
                   ),
                 ),
-              ],
+              ),
             )
-            */
           ],
         ),
       ),
     );
   }
+
+  final bool automaticallyImplyLeading;
 }
