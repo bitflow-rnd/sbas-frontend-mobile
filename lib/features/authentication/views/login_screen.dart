9493e868299e44173cc5062402d4bf19aa1d8660
woo4826 <woo4826@gmail.com>
2023-04-14 20:56:19 +0900

    remove unused import

--- lib/features/authentication/views/login_screen.dart
+++ lib/features/authentication/views/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:sbas/common/Common.dart';
 import 'package:sbas/common/bitflow_theme.dart';
 import 'package:sbas/constants/gaps.dart';
 import 'package:sbas/constants/palette.dart';
