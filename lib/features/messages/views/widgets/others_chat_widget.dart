8fb7f0225e178bd7d8918d95bbc7293d795dd502
haksung59 <hak9501@gmail.com>
2023-04-25 15:17:54 +0900

    채팅방 구현

--- lib/features/messages/views/widgets/others_chat_widget.dart
+++ lib/features/messages/views/widgets/others_chat_widget.dart
@@ -1,86 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:sbas/constants/gaps.dart';
 import 'package:sbas/features/messages/models/talk_msg_model.dart';
 import 'package:sbas/features/messages/views/widgets/talk_room_widget.dart';
 
-ListView othersChatWidget(
-  AsyncSnapshot<List<TalkMsgModel>> snapshot,
+Row othersChatWidget(
+  TalkMsgModel input,
   ScrollController scrollController,
 ) {
-  return ListView.builder(
-    controller: scrollController,
-    itemCount: snapshot.data!.length,
-    itemBuilder: (context, index) {
-      var chat = snapshot.data![index];
-      String? userImage = 'assets/message/doctor_icon.png';
+  var chat = input;
+  String? userImage = 'assets/message/doctor_icon.png';
 
-      return Padding(
-        padding: const EdgeInsets.only(top: 6.5),
-        child: Row(
-          children: [
-            Flexible(
-              flex: 3,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage(userImage),
-                          radius: 20,
-                        ),
-                      ),
-                      Text(
-                        chat.rgstUserId!,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // Container(
-                  // padding: padding,
-                  // child:
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 8),
-                          child: Text(
-                            chat.msg ?? '',
-                            style: const TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Text(
-                          formatDateTime(chat.updtDttm!),
-                          style: const TextStyle(
-                              fontSize: 8.0, color: Colors.grey),
-                        ),
-                      ),
-                    ],
-                  ),
-                  // ),
-                ],
-              ),
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Gaps.h16,
+      CircleAvatar(
+        backgroundImage: AssetImage(userImage),
+        radius: 20,
+      ),
+      Gaps.h10,
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            chat.rgstUserId!,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 13,
             ),
-          ],
-        ),
-      );
-    },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                constraints: const BoxConstraints(
+                  maxWidth: 260,
+                ),
+                padding: const EdgeInsets.only(top: 10),
+                child: Container(
+                  margin: const EdgeInsets.only(right: 5),
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(8),
+                        topRight: Radius.circular(8),
+                        bottomLeft: Radius.circular(8)),
+                    color: Colors.white,
+                  ),
+                  child: Text(input.msg ?? '',
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'OverpassRegular',
+                          fontWeight: FontWeight.w300)),
+                ),
+              ),
+              Text(
+                textAlign: TextAlign.left,
+                formatDateTime(input.updtDttm!),
+                style: const TextStyle(fontSize: 8.0, color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
   );
 }
