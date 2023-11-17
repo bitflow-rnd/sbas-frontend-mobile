ef4cebe4f60183d03d6c0c9dcd13317b506fbe6f
jiseong <jeff3877@gmail.com>
2023-11-17 12:20:23 +0900

    private upload 추가

--- lib/common/providers/base_attc_provider.dart
+++ lib/common/providers/base_attc_provider.dart
@@ -1,5 +1,8 @@
+import 'package:dio/dio.dart' as dio;
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sbas/common/api/v1_provider.dart';
-
+import '../../util.dart';
 import '../models/base_attc_model.dart';
 
 class BaseAttcProvider {
@@ -21,8 +24,40 @@ class BaseAttcProvider {
     await _api.downloadPublicFile(attcGrpId, attcId, fileNm);
   }
 
+  Future<List<dynamic>> uploadPrivateImage(dio.MultipartFile file) async {
+    final client = dio.Dio();
+
+    try {
+      client.options.contentType = 'multipart/form-data';
+      client.options.headers = authToken;
+
+      final res = await client.postUri(
+        Uri.parse('$_privateBaseUrl/upload'),
+        data: dio.FormData.fromMap(
+          {
+            'param1': '',
+            'param2': file,
+          },
+        ),
+      );
+      if (res.statusCode == 200) {
+        return res.data['result']['attcId'];
+      }
+    } catch (exception) {
+      if (kDebugMode) {
+        print({
+          'exception': exception,
+        });
+      }
+    } finally {
+      client.close();
+    }
+    throw ArgumentError();
+  }
+
   final String _privateRoute = 'private/common';
   final String _publicRoute = 'public/common';
+  final String _privateBaseUrl = '${dotenv.env['BASE_URL']}/v1/private/common';
 
   final _api = V1Provider();
 }
--- lib/common/repos/file_repo.dart
+++ lib/common/repos/file_repo.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sbas/common/models/base_attc_model.dart';
 
 import '../providers/base_attc_provider.dart';
@@ -12,6 +14,13 @@ class FileRepository {
 
   Future<void> downloadPublicFile(String attcGrpId, String attcId, String fileNm) async =>
       await _fileProvider.downloadPublicFile(attcGrpId, attcId, fileNm);
+
+  Future<List<dynamic>> uploadPrivateImage(XFile file) async => await _fileProvider.uploadPrivateImage(
+    await MultipartFile.fromFile(
+      file.path,
+      filename: file.name,
+    ),
+  );
 
   final _fileProvider = BaseAttcProvider();
 }
