b8b27b5184550821185c4a896bf7f87e35576a11
haksung <hak9501@gmail.com>
2023-12-05 15:21:45 +0900

    my photo chat 이미지 크기조정,여러개 파일의 경우 추가

--- lib/features/messages/views/widgets/my_chat_widget.dart
+++ lib/features/messages/views/widgets/my_chat_widget.dart
@@ -28,70 +28,65 @@ Row myChatPhotoAttachedWidget(
         style: CTS(fontSize: 12, color: Colors.grey),
       ),
       Gaps.h5,
-      Container(
-        constraints: const BoxConstraints(
-          maxWidth: 310,
-        ),
-        padding: const EdgeInsets.only(top: 10),
+      Flexible(
         child: Container(
-          margin: const EdgeInsets.only(right: 15),
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(8),
-                topRight: Radius.circular(8),
-                bottomLeft: Radius.circular(8)),
-            color: const Color(0xfffff700).withOpacity(0.333),
+          constraints: const BoxConstraints(
+            maxWidth: 310,
           ),
-          child: Column(
-            children: [
-              FutureBuilder(
-                future: getFileList(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Image.network(
-                      "http://dev.smartbas.org/${snapshot.data!.first.uriPath}/${snapshot.data!.first.fileNm}",
-                      height: 70.h,
-                      width: 100.w,
-                    );
-                  } else if (snapshot.hasError) {
-                    return Image.asset(
-                      "assets/auth_group/image_location_small.png",
-                      height: 70.h,
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                },
-              ),
-              // Image.asset(
-              //   "assets/auth_group/image_location_small.png",
-              //   height: 70.h,
-              //   width: 80.w,
-              // ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    input.msg ?? '',
-                    textAlign: TextAlign.start,
-                    style: CTS(
-                      color: Colors.black,
-                      fontSize: 13,
+          padding: const EdgeInsets.only(top: 10),
+          child: Container(
+            margin: const EdgeInsets.only(right: 15),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                  bottomLeft: Radius.circular(8)),
+              color: const Color(0xfffff700).withOpacity(0.333),
+            ),
+            child: Column(
+              children: [
+                FutureBuilder(
+                  future: getFileList(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Wrap(
+                        spacing: 0.8,
+                        runSpacing: 0.8,
+                        children: snapshot.data!
+                            .map((file) => Image.network(
+                                  "http://dev.smartbas.org/${file.uriPath}/${file.fileNm}",
+                                  height: 150.h,
+                                  width: 100.w,
+                                ))
+                            .toList(),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Image.asset(
+                        "assets/auth_group/image_location_small.png",
+                        height: 70.h,
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      input.msg ?? '',
+                      textAlign: TextAlign.start,
+                      style: CTS(
+                        color: Colors.black,
+                        fontSize: 13,
+                      ),
                     ),
-                  ),
-                  Gaps.h5,
-                  // Text(
-                  //   '2.3MB',
-                  //   textAlign: TextAlign.start,
-                  //   style: CTS(
-                  //     color: Palette.greyText_60,
-                  //     fontSize: 12,
-                  //   ),
-                  // ),
-                ],
-              ),
-            ],
+                    Gaps.h5,
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
