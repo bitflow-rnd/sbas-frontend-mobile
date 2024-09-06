112fd64f4ba32eed173b358dc6db191080f78daf
김학성 <hak9501@gmail.com>
2024-09-06 15:58:26 +0900

    모달창 분리

--- lib/features/lookup/views/hospital_bed_request_screen_v2.dart
+++ lib/features/lookup/views/hospital_bed_request_screen_v2.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
@@ -11,24 +10,23 @@ import 'package:sbas/constants/palette.dart';
 import 'package:sbas/features/assign/bloc/assign_bed_bloc.dart';
 import 'package:sbas/features/assign/repos/assign_repo.dart';
 import 'package:sbas/features/assign/views/assign_bed_screen.dart';
-import 'package:sbas/features/assign/views/widgets/request/assign_origin_infomationV2.dart';
 import 'package:sbas/features/assign/views/widgets/request/assign_infectious_diseaseV2.dart';
+import 'package:sbas/features/assign/views/widgets/request/assign_origin_infomationV2.dart';
 import 'package:sbas/features/assign/views/widgets/request/assign_severely_diseaseV2.dart';
+import 'package:sbas/features/assign/views/widgets/request/patient_reg_info_widget_v2.dart';
 import 'package:sbas/features/lookup/blocs/hospital_bed_request_bloc.dart';
 import 'package:sbas/features/lookup/blocs/infectious_disease_bloc.dart';
 import 'package:sbas/features/lookup/blocs/patient_register_bloc.dart';
-import 'package:sbas/features/lookup/presenters/severely_disease_presenter.dart';
+import 'package:sbas/features/lookup/models/patient_duplicate_check_model.dart';
 import 'package:sbas/features/lookup/models/patient_model.dart';
 import 'package:sbas/features/lookup/models/patient_reg_info_model.dart';
 import 'package:sbas/features/lookup/presenters/origin_info_presenter.dart';
+import 'package:sbas/features/lookup/presenters/severely_disease_presenter.dart';
 import 'package:sbas/features/lookup/repos/patient_repo.dart';
-import 'package:sbas/features/assign/views/widgets/request/patient_reg_info_widget_v2.dart';
+import 'package:sbas/features/lookup/views/patient_duplicate_check_modal.dart';
 import 'package:sbas/features/lookup/views/widgets/paitent_reg_info_modal.dart';
 import 'package:sbas/features/lookup/views/widgets/patient_reg_report_widget.dart';
 import 'package:sbas/features/lookup/views/widgets/patient_top_info_widget.dart';
-import 'package:sbas/constants/common.dart';
-import 'package:sbas/constants/gaps.dart';
-import 'package:sbas/features/lookup/models/patient_duplicate_check_model.dart';
 
 final assignNewBedProvider =
     AsyncNotifierProvider<AssignNewBedPresenter, PatientRegInfoModel>(
@@ -261,8 +259,7 @@ class HospitalBedRequestScreenV2 extends ConsumerWidget {
             ? Expanded(
                 child: InkWell(
                   onTap: () {
-                    ref
-                        .read(orderOfRequestProvider.notifier)
+                    ref.read(orderOfRequestProvider.notifier)
                         .update((state) => order - 1);
                   },
                   child: Container(
@@ -517,239 +514,8 @@ class HospitalBedRequestScreenV2 extends ConsumerWidget {
     );
   }
 
-  patientDuplicateCheckModal(context, PatientCheckResponse oldPatient,
-      PatientRegInfoModel newPatient, WidgetRef ref) {
-    final order = ref.watch(orderOfRequestProvider);
-    return Common.showModal(
-        context,
-        IntrinsicWidth(
-          child: Dialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.r)),
-            backgroundColor: Palette.white,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24.r),
-              child: IntrinsicHeight(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 4.h),
-                    Container(
-                      margin: EdgeInsets.only(top: 24.h),
-                      height: 44.h,
-                      child: Center(
-                        child: Image.asset(
-                          "assets/auth_group/modal_check.png",
-                          width: 44.h,
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 16.r, bottom: 12.r),
-                            child: Text(
-                              "환자정보 존재",
-                              textAlign: TextAlign.center,
-                              style: CTS.bold(
-                                  color: Palette.black,
-                                  fontSize: 14,
-                                  height: 1.5),
-                            ).c,
-                          ),
-                        ),
-                      ],
-                    ),
-                    modal2frag(oldPatient.ptNm == newPatient.ptNm,
-                        "이름 : ${oldPatient.ptNm}"),
-                    modal2frag(
-                      oldPatient.rrno1 == newPatient.rrno1 &&
-                          oldPatient.rrno2 == newPatient.rrno2,
-                      "주민등록번호 : ${oldPatient.rrno1}-${oldPatient.rrno2}",
-                    ),
-                    modal2frag(
-                      oldPatient.dstr1Cd == newPatient.dstr1Cd &&
-                          oldPatient.dstr2Cd == newPatient.dstr2Cd,
-                      "주소 : ${oldPatient.dstr1CdNm} ${oldPatient.dstr2CdNm}",
-                    ),
-                    modal2frag(oldPatient.telno == newPatient.telno,
-                        "연락처 : ${oldPatient.telno}"),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.r),
-                      child: Text(
-                        "※ 동명이인 여부를 확인해주세요.",
-                        textAlign: TextAlign.start,
-                        style: CTS(color: Palette.mainColor, fontSize: 12),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 16.r),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(6.r),
-                              child: Material(
-                                color: Palette.white,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(
-                                      color: Color(0xff676a7a),
-                                      width: 1.w,
-                                    ),
-                                    borderRadius: BorderRadius.circular(6.r),
-                                  ),
-                                  child: InkWell(
-                                    onTap: () {
-                                      Navigator.pop(context, "close");
-                                    },
-                                    child: Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 9.r),
-                                      child: Text(
-                                        "닫기",
-                                        style: CTS(
-                                            color: Color(0xff676a7a),
-                                            fontSize: 14),
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 1,
-                                      ).c,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 10.w),
-                          Expanded(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(6.r),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.transparent,
-                                  border: Border.all(
-                                    color: Palette.mainColor,
-                                    width: 1.w,
-                                  ),
-                                  borderRadius: BorderRadius.circular(6.r),
-                                ),
-                                child: Material(
-                                  color: Colors.transparent,
-                                  child: InkWell(
-                                    onTap: () {
-                                      ref
-                                          .read(patientRegProvider.notifier)
-                                          .updatePatient(
-                                              oldPatient.ptId, context);
-                                      Navigator.pop(context);
-                                      ref
-                                          .read(orderOfRequestProvider.notifier)
-                                          .update((state) => order + 1);
-                                    },
-                                    child: Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 9.r),
-                                      child: Text(
-                                        "업데이트",
-                                        style: CTS(
-                                            color: Palette.mainColor,
-                                            fontSize: 14),
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 1,
-                                      ).c,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 10.w),
-                          Expanded(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(6.r),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Palette.mainColor,
-                                  border: Border.all(
-                                    color: Palette.mainColor,
-                                    width: 1.w,
-                                  ),
-                                  borderRadius: BorderRadius.circular(6.r),
-                                ),
-                                child: Material(
-                                  color: Palette.mainColor,
-                                  child: InkWell(
-                                    onTap: () {
-                                      ref
-                                          .read(patientRegProvider.notifier)
-                                          .registry(newPatient.ptId, context);
-                                      Navigator.pop(context);
-                                      ref
-                                          .read(orderOfRequestProvider.notifier)
-                                          .update((state) => order + 1);
-                                    },
-                                    child: Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 9.r),
-                                      child: Text(
-                                        "새로등록",
-                                        style: CTS(
-                                            color: Palette.white, fontSize: 14),
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 1,
-                                      ).c,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ));
-  }
-
-  modal2frag(bool isCorrect, String detail) {
-    return Container(
-      margin: EdgeInsets.only(top: 9.h),
-      child: Row(
-        children: [
-          Container(
-            width: 44.w,
-            padding: EdgeInsets.symmetric(vertical: 2.h),
-            decoration: BoxDecoration(
-              color:
-                  Color(isCorrect ? 0xff538ef5 : 0xff676a7a).withOpacity(0.12),
-              borderRadius: BorderRadius.circular(4),
-            ),
-            child: Text(
-              isCorrect ? '일치' : "불일치",
-              style: CTS(
-                color: Color(isCorrect ? 0xff538ef5 : 0xff676a7a),
-                fontSize: 12,
-              ),
-              overflow: TextOverflow.ellipsis,
-            ).c,
-          ),
-          Gaps.h8,
-          Text(
-            detail,
-            style: CTS(color: Colors.black, fontSize: 12),
-          ),
-        ],
-      ),
-    );
-  }
-
   bool tryBasicInfoValidation(WidgetRef ref) {
+    print('patientBasicForm');
     bool isValid = patientBasicFormKey.currentState?.validate() ?? false;
 
     if (isValid) {
@@ -759,6 +525,7 @@ class HospitalBedRequestScreenV2 extends ConsumerWidget {
   }
 
   bool tryInfectDisValidation() {
+    print('infectiousDisForm');
     bool isValid = infectiousDisFormKey.currentState?.validate() ?? false;
 
     if (isValid) {
@@ -768,6 +535,7 @@ class HospitalBedRequestScreenV2 extends ConsumerWidget {
   }
 
   bool trySeverelyDisValidation(WidgetRef ref) {
+    print('severelyDisForm');
     print(severelyDisFormKey.currentState?.validate());
     bool isValid = severelyDisFormKey.currentState?.validate() ?? false;
 
@@ -780,6 +548,7 @@ class HospitalBedRequestScreenV2 extends ConsumerWidget {
   }
 
   bool tryOrignInfoValidation(WidgetRef ref) {
+    print('originForm');
     bool isValid = originFormKey.currentState?.validate() ?? false;
     // isValid = ref.watch(severelyDiseaseProvider.notifier).isValid();
     // isValid = ref.watch(originInfoProvider.notifier).isValid();
