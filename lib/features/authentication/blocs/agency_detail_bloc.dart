f2d547fa6322af25923f06519531680a93cd3bfe
jiseong <jeff3877@gmail.com>
2024-09-27 10:37:48 +0900

    보건소 목록 나오게 수정

--- lib/features/authentication/blocs/agency_detail_bloc.dart
+++ lib/features/authentication/blocs/agency_detail_bloc.dart
@@ -6,14 +6,14 @@ import 'package:sbas/features/authentication/blocs/agency_region_bloc.dart';
 import 'package:sbas/features/authentication/blocs/user_reg_bloc.dart';
 import 'package:sbas/features/authentication/models/info_inst_model.dart';
 import 'package:sbas/features/authentication/repos/user_reg_req_repo.dart';
+import 'package:sbas/features/lookup/blocs/patient_register_bloc.dart';
 
 class AgencyDetailBloc extends AsyncNotifier<List<InfoInstModel>> {
   @override
   FutureOr<List<InfoInstModel>> build() async {
     _infoInstRepository = ref.read(userRegReqProvider);
-
-    list = await _infoInstRepository.getOrganCode('', '', '');
-
+    var dstr1cd = ref.watch(patientRegProvider.notifier).patientInfoModel.dstr1Cd;
+    list = await _infoInstRepository.getOrganCode('ORGN0003', dstr1cd, null);
     return list;
   }
 
